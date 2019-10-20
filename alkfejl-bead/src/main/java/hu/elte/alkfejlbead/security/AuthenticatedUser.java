6c776a94bb5fedb0b2809086672e66ed8138d70a
heymisi <53046632+heymisi@users.noreply.github.com>
2019-10-20 19:59:26 +0200

    Update AuthenticatedUser.java

--- alkfejl-bead/src/main/java/hu/elte/alkfejlbead/security/AuthenticatedUser.java
+++ alkfejl-bead/src/main/java/hu/elte/alkfejlbead/security/AuthenticatedUser.java
@@ -1,25 +1,16 @@
 package hu.elte.alkfejlbead.security;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- *//*
 
-package hu.elte.issuetracker.security;
 
-import hu.elte.issuetracker.entities.Student;
+import hu.elte.alkfejlbead.entities.Student;
+import hu.elte.alkfejlbead.entities.Teacher;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-*/
-/**
- *
- * @author KeresztiKrisztián
- *//*
+
 
 @RequestScope
 @Component
@@ -29,4 +20,3 @@ import org.springframework.web.context.annotation.RequestScope;
 public class AuthenticatedUser {
     private Student user;
 }
-*/
